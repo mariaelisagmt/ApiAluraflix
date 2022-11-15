e818bf48236d71aa436092c3ca283d03dfee8218
Maria Elisa <maria.gomes@myrp.com.br>
2022-11-15 18:35:00 -0300

    Add database

---
+++ APIAluraflix/Data/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIAluraflix.Models;
+
+namespace APIAluraflix.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Video> Videos {get;set;}
+    }
+}
